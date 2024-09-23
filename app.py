d73239d63d45f48ae44de67eadde77a66b8677ee
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-09-22 23:22:22 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,9 @@
 # Fecha: 22 Septiembre 2024
 # Descripción: Código para rutas gps v2.
 # GitHub: https://github.com/Jona163
+
+from flask import Flask, render_template, request, jsonify
+from math import radians, sin, cos, sqrt, atan2, exp
+import random
+
+app = Flask(__name__)
